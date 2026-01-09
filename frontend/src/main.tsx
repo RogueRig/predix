14b950abe5f11091c91f36bbe25b973994825aab
RogueRig <58204452+RogueRig@users.noreply.github.com>
2026-01-10 00:20:11 +0530

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,10 +3,25 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AppPrivyProvider } from "./PrivyProvider";
 
+function Root() {
+  try {
+    return (
+      <AppPrivyProvider>
+        <App />
+      </AppPrivyProvider>
+    );
+  } catch (e) {
+    return (
+      <div style={{ padding: 24, color: "red" }}>
+        <h2>Predix crashed</h2>
+        <pre>{String(e)}</pre>
+      </div>
+    );
+  }
+}
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <AppPrivyProvider>
-      <App />
-    </AppPrivyProvider>
+    <Root />
   </React.StrictMode>
 );
