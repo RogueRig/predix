26202b47c5cf3168300dcac07d245f225da7bc4b
RogueRig <58204452+RogueRig@users.noreply.github.com>
2026-01-10 00:17:13 +0530

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { AppPrivyProvider } from "./PrivyProvider";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <AppPrivyProvider>
+      <App />
+    </AppPrivyProvider>
   </React.StrictMode>
 );
