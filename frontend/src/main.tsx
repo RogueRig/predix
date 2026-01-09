a734be1010477184b6a281ea4fbf20cc77c6754c
RogueRig <58204452+RogueRig@users.noreply.github.com>
2026-01-09 23:54:50 +0530

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './styles.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { PrivyProvider } from "@privy-io/react-auth";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <PrivyProvider
+      appId={import.meta.env.VITE_PRIVY_APP_ID}
+      config={{
+        loginMethods: ["email", "wallet", "farcaster"],
+        appearance: { theme: "dark" },
+      }}
+    >
+      <App />
+    </PrivyProvider>
+  </React.StrictMode>
+);
