884c514115986bd3d30cfd70acaf698fa7994ede
RogueRig <58204452+RogueRig@users.noreply.github.com>
2026-01-10 00:11:39 +0530

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,17 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-function Test() {
-  return (
-    <div style={{ padding: 24 }}>
-      <h1>Predix is alive ✅</h1>
-      <p>If you see this, React is working.</p>
-    </div>
-  );
-}
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Test />
+    <App />
   </React.StrictMode>
 );
